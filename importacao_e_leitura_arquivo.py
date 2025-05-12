13b0816d7d65c9d9da4b608c203e4ef374cbcd44
Douglas Ribeiro <143748536+Douglasr7007@users.noreply.github.com>
2025-05-11 23:14:58 -0300

    Adiciona dados e script de importação e leitura do DataFrame

---
+++ importacao_e_leitura_arquivo.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+base_dados = pd.read_csv("C:\\Analise_Aducacional\\Dados\\StudentsPerformance.csv")
+
